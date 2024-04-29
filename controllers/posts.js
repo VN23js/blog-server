4846a7df86a50b33cae73a69611f85bb8fac27a4
Ivan <ivannikolae2000@gmail.com>
2024-04-29 23:31:10 +0300

    new

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,3 +48,19 @@ export const createPost = async (req, res) => {
   }
 };
 ////////////////////////////////////////////////////////////////
+//GET ALL /posts
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await Post.find().sort("-createdAt");
+    const popularPosts = await Post.find().limit(5).sort("-views");
+
+    if (!posts) {
+      return res.json({ message: "Постов нет" });
+    }
+
+    res.json({ posts, popularPosts });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 import { checkAuth } from "../utils/checkAuth.js";
-import { createPost } from "../controllers/posts.js";
+import { createPost, getAll } from "../controllers/posts.js";
 import { isAdmin } from "../controllers/auth.js";
 /////////////
 ///Create Post
 router.post("/", checkAuth, isAdmin, createPost);
-///////////
+//////////// GET POST
+router.get("/", getAll);
+
 export default router;
