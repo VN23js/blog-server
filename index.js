f17104b6b022239c141d97ff6ef01f781fb25401
Ivan <ivannikolae2000@gmail.com>
2024-05-27 18:42:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import fileUpload from "express-fileupload";
 const app = express();
 dotenv.config();
 const corsOptions = {
-  origin: "https://blog-frontend-83lj8ppj6-vn23js-projects.vercel.app",
+  origin: "https://blog-frontend-1c87cyosa-vn23js-projects.vercel.app",
   optionsSuccessStatus: 200
 };
 //Constants
